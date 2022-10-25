18b3294c11945421a49db7e637e561ac83e4ac8b
alexfortin <fortin.alex14@gmail.com>
2022-10-24 21:06:10 -0500

    set up 2 routes for the html

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const {notes} = require("./data/db.json")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"))
 
 //displays all notes data
 app.get("/notes", (req, res) => {
@@ -36,6 +37,15 @@ app.delete("/notes/:id", (req, res) => {
     const newArray = notes.filter(note => note.id != parseInt(req.params.id))
     fs.writeFileSync(path.join(__dirname, "./data/db.json"), JSON.stringify({notes}), null, 2 )
     res.json(newArray)
+})
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
+})
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"))
 })
 
 
